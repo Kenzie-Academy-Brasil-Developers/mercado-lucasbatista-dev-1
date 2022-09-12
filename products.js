8aeb86f0a38565ac798a2aa69f091642f61d5cc0
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-09-12 18:26:45 -0300

    adicionando a tag main pelo js

--- products.js
+++ products.js
@@ -88,6 +88,45 @@ const products = [
     imageDescription: "",
   },
 ];
+let body = document.body;
+let tagMain = document.createElement("main");
+tagMain.classList.add("container");
+let tagsection1 = document.createElement("section");
+tagsection1.classList.add("products-section");
+let tagsection2 = document.createElement("section");
+tagsection2.classList.add("products-section");
+let tagsection3 = document.createElement("section");
+tagsection3.classList.add("products-section");
+let tagH1Frutas = document.createElement("h1");
+tagH1Frutas.innerText = "Frutas";
+let tagH1Bebidas = document.createElement("h1");
+tagH1Bebidas.innerText = "Bebidas";
+let tagH1Higiene = document.createElement("h1");
+tagH1Higiene.innerText = "Higiene";
+let tagDiv1 = document.createElement("div");
+tagDiv1.classList.add("products-content");
+let tagDiv2 = document.createElement("div");
+tagDiv2.classList.add("products-content");
+let tagDiv3 = document.createElement("div");
+tagDiv3.classList.add("products-content");
+let tagUl1 = document.createElement("ul");
+tagUl1.classList.add("ul_fruits");
+let tagUl2 = document.createElement("ul");
+tagUl2.classList.add("ul_drinks");
+let tagUl3 = document.createElement("ul");
+tagUl3.classList.add("ul_hygiene");
+
+body.appendChild(tagMain);
+tagMain.append(tagsection1);
+tagsection1.append(tagH1Frutas, tagDiv1);
+tagDiv1.appendChild(tagUl1);
+tagMain.append(tagsection2);
+tagsection2.append(tagH1Bebidas, tagDiv2);
+tagDiv2.appendChild(tagUl2);
+tagMain.append(tagsection3);
+tagsection3.append(tagH1Higiene, tagDiv3);
+tagDiv3.appendChild(tagUl3);
+console.log(tagMain);
 
 function FiltrarProdutos(listaProdutos) {
   for (let i = 0; i < listaProdutos.length; i++) {
